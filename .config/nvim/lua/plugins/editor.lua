72a3dcb84996aba5226a4e39460e51f311b67241
badAl <badalsingh120403@gmail.com>
2024-11-09 02:26:44 +0530

    feat: install snacks remove nvim-notify

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,186 +1,204 @@
 local pick = function()
-  if LazyVim.pick.picker.name == "telescope" then
-    return require("telescope").extensions.refactoring.refactors()
-  elseif LazyVim.pick.picker.name == "fzf" then
-    local fzf_lua = require("fzf-lua")
-    local results = require("refactoring").get_refactors()
-    local refactoring = require("refactoring")
+    if LazyVim.pick.picker.name == "telescope" then
+        return require("telescope").extensions.refactoring.refactors()
+    elseif LazyVim.pick.picker.name == "fzf" then
+        local fzf_lua = require("fzf-lua")
+        local results = require("refactoring").get_refactors()
+        local refactoring = require("refactoring")
 
-    local opts = {
-      fzf_opts = {},
-      fzf_colors = true,
-      actions = {
-        ["default"] = function(selected)
-          refactoring.refactor(selected[1])
-        end,
-      },
-    }
-    fzf_lua.fzf_exec(results, opts)
-  end
+        local opts = {
+            fzf_opts = {},
+            fzf_colors = true,
+            actions = {
+                ["default"] = function(selected)
+                    refactoring.refactor(selected[1])
+                end,
+            },
+        }
+        fzf_lua.fzf_exec(results, opts)
+    end
 end
 
 return {
 
-  -- which key
-  { "folke/which-key.nvim", enabled = false },
+    -- which key
+    { "folke/which-key.nvim", enabled = false },
 
-  -- disable flash
-  {
-    "folke/flash.nvim",
-    enabled = false,
-  },
+    -- disable flash
+    {
+        "folke/flash.nvim",
+        enabled = false,
+    },
 
-  -- neo-tree
-  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+    -- indent-blankline.nvim
+    { "lukas-reineke/indent-blankline.nvim", enabled = false },
 
-  -- grug-far.nvim -> search/replace in multiple files
-  { "MagicDuck/grug-far.nvim", enabled = false },
+    -- neo-tree
+    { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 
-  -- persistence.nvim
-  { "folke/persistence.nvim", enabled = false },
+    -- grug-far.nvim -> search/replace in multiple files
+    { "MagicDuck/grug-far.nvim", enabled = false },
 
-  -- nvim-cmp
-  {
-    "hrsh7th/nvim-cmp",
-    opts = {
-      window = {
-        completion = {
-          border = "rounded",
-          scrollbar = "║",
-          winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
+    -- persistence.nvim
+    { "folke/persistence.nvim", enabled = false },
+
+    -- snacks.nvim
+    {
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        opts = {
+            notifier = {
+                enabled = true,
+                timeout = 1000,
+            },
+
+            style = "minimal",
         },
-        documentation = {
-          border = "rounded",
-          scrollbar = "║",
-          winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
-        },
-      },
     },
-  },
 
-  -- refactoring-nvim
-  {
-    "ThePrimeagen/refactoring.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
+    -- nvim-cmp
+    {
+        "hrsh7th/nvim-cmp",
+        opts = {
+            window = {
+                completion = {
+                    border = "rounded",
+                    scrollbar = "║",
+                    winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
+                },
+                documentation = {
+                    border = "rounded",
+                    scrollbar = "║",
+                    winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
+                },
+            },
+        },
     },
-    keys = {
-      { "<leader>r", "", desc = "+refactor", mode = { "n", "v" } },
-      {
-        "<leader>rs",
-        pick,
-        mode = "v",
-        desc = "Refactor",
-      },
-      {
-        "<leader>ri",
-        function()
-          require("refactoring").refactor("Inline Variable")
+
+    -- refactoring-nvim
+    {
+        "ThePrimeagen/refactoring.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        keys = {
+            { "<leader>r", "", desc = "+refactor", mode = { "n", "v" } },
+            {
+                "<leader>rs",
+                pick,
+                mode = "v",
+                desc = "Refactor",
+            },
+            {
+                "<leader>ri",
+                function()
+                    require("refactoring").refactor("Inline Variable")
+                end,
+                mode = { "n", "v" },
+                desc = "Inline Variable",
+            },
+            {
+                "<leader>rb",
+                function()
+                    require("refactoring").refactor("Extract Block")
+                end,
+                desc = "Extract Block",
+            },
+            {
+                "<leader>rf",
+                function()
+                    require("refactoring").refactor("Extract Block To File")
+                end,
+                desc = "Extract Block To File",
+            },
+            {
+                "<leader>rP",
+                function()
+                    require("refactoring").debug.printf({ below = false })
+                end,
+                desc = "Debug Print",
+            },
+            {
+                "<leader>rp",
+                function()
+                    require("refactoring").debug.print_var({ normal = true })
+                end,
+                desc = "Debug Print Variable",
+            },
+            {
+                "<leader>rc",
+                function()
+                    require("refactoring").debug.cleanup({})
+                end,
+                desc = "Debug Cleanup",
+            },
+            {
+                "<leader>rf",
+                function()
+                    require("refactoring").refactor("Extract Function")
+                end,
+                mode = "v",
+                desc = "Extract Function",
+            },
+            {
+                "<leader>rF",
+                function()
+                    require("refactoring").refactor("Extract Function To File")
+                end,
+                mode = "v",
+                desc = "Extract Function To File",
+            },
+            {
+                "<leader>rx",
+                function()
+                    require("refactoring").refactor("Extract Variable")
+                end,
+                mode = "v",
+                desc = "Extract Variable",
+            },
+            {
+                "<leader>rp",
+                function()
+                    require("refactoring").debug.print_var()
+                end,
+                mode = "v",
+                desc = "Debug Print Variable",
+            },
+        },
+        opts = {
+            prompt_func_return_type = {
+                go = false,
+                java = false,
+                cpp = false,
+                c = false,
+                h = false,
+                hpp = false,
+                cxx = false,
+            },
+            prompt_func_param_type = {
+                go = false,
+                java = false,
+                cpp = false,
+                c = false,
+                h = false,
+                hpp = false,
+                cxx = false,
+            },
+            printf_statements = {},
+            print_var_statements = {},
+            show_success_message = true, -- shows a message with information about the refactor on success
+            -- i.e. [Refactor] Inlined 3 variable occurrences
+        },
+        config = function(_, opts)
+            require("refactoring").setup(opts)
+            if LazyVim.has("telescope.nvim") then
+                LazyVim.on_load("telescope.nvim", function()
+                    require("telescope").load_extension("refactoring")
+                end)
+            end
         end,
-        mode = { "n", "v" },
-        desc = "Inline Variable",
-      },
-      {
-        "<leader>rb",
-        function()
-          require("refactoring").refactor("Extract Block")
-        end,
-        desc = "Extract Block",
-      },
-      {
-        "<leader>rf",
-        function()
-          require("refactoring").refactor("Extract Block To File")
-        end,
-        desc = "Extract Block To File",
-      },
-      {
-        "<leader>rP",
-        function()
-          require("refactoring").debug.printf({ below = false })
-        end,
-        desc = "Debug Print",
-      },
-      {
-        "<leader>rp",
-        function()
-          require("refactoring").debug.print_var({ normal = true })
-        end,
-        desc = "Debug Print Variable",
-      },
-      {
-        "<leader>rc",
-        function()
-          require("refactoring").debug.cleanup({})
-        end,
-        desc = "Debug Cleanup",
-      },
-      {
-        "<leader>rf",
-        function()
-          require("refactoring").refactor("Extract Function")
-        end,
-        mode = "v",
-        desc = "Extract Function",
-      },
-      {
-        "<leader>rF",
-        function()
-          require("refactoring").refactor("Extract Function To File")
-        end,
-        mode = "v",
-        desc = "Extract Function To File",
-      },
-      {
-        "<leader>rx",
-        function()
-          require("refactoring").refactor("Extract Variable")
-        end,
-        mode = "v",
-        desc = "Extract Variable",
-      },
-      {
-        "<leader>rp",
-        function()
-          require("refactoring").debug.print_var()
-        end,
-        mode = "v",
-        desc = "Debug Print Variable",
-      },
     },
-    opts = {
-      prompt_func_return_type = {
-        go = false,
-        java = false,
-        cpp = false,
-        c = false,
-        h = false,
-        hpp = false,
-        cxx = false,
-      },
-      prompt_func_param_type = {
-        go = false,
-        java = false,
-        cpp = false,
-        c = false,
-        h = false,
-        hpp = false,
-        cxx = false,
-      },
-      printf_statements = {},
-      print_var_statements = {},
-      show_success_message = true, -- shows a message with information about the refactor on success
-      -- i.e. [Refactor] Inlined 3 variable occurrences
-    },
-    config = function(_, opts)
-      require("refactoring").setup(opts)
-      if LazyVim.has("telescope.nvim") then
-        LazyVim.on_load("telescope.nvim", function()
-          require("telescope").load_extension("refactoring")
-        end)
-      end
-    end,
-  },
 }
--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,51 +1,51 @@
 local Util = require("lazyvim.util")
 return {
 
-  -- bufferline disable
-  { "akinsho/bufferline.nvim", enabled = false },
+    -- bufferline disable
+    { "akinsho/bufferline.nvim", enabled = false },
 
-  -- lualine
-  { "nvim-lualine/lualine.nvim", enabled = false },
+    -- lualine
+    { "nvim-lualine/lualine.nvim", enabled = false },
 
-  -- dashboard
-  { "nvimdev/dashboard-nvim", enabled = false },
+    -- dashboard
+    { "nvimdev/dashboard-nvim", enabled = false },
 
-  -- mini-icons
-  { "echasnovski/mini.icons", enabled = false },
+    -- mini-icons
+    { "echasnovski/mini.icons", enabled = false },
 
-  -- noice
-  {
-    "folke/noice.nvim",
-    opts = {
-      presets = {
-        lsp_doc_border = true,
-      },
-      routes = {
-        {
-          view = "cmdline",
-          filter = { event = "msg_showmode" },
+    -- noice
+    {
+        "folke/noice.nvim",
+        opts = {
+            presets = {
+                lsp_doc_border = true,
+            },
+            routes = {
+                {
+                    view = "cmdline",
+                    filter = { event = "msg_showmode" },
+                },
+            },
         },
-      },
     },
-  },
 
-  -- notify
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      timeout = 1000,
-      render = "compact",
-      top_down = false,
-    },
-  },
+    -- notify
+    -- {
+    --   "rcarriga/nvim-notify",
+    --   opts = {
+    --     timeout = 1000,
+    --     render = "compact",
+    --     top_down = false,
+    --   },
+    -- },
 
-  -- mini-indentscope
-  {
-    "echasnovski/mini.indentscope",
-    version = false, -- wait till new 0.7.0 release to put it back on semver
-    event = "LazyFile",
-    opts = {
-      options = { try_as_border = true },
+    -- mini-indentscope
+    {
+        "echasnovski/mini.indentscope",
+        version = false, -- wait till new 0.7.0 release to put it back on semver
+        event = "LazyFile",
+        opts = {
+            options = { try_as_border = true },
+        },
     },
-  },
 }
