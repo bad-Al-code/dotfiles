0b392fff157af42e78de8723e6bb6d2cd3054b0a
Badal <badalsingh120403@gmail.com>
2025-11-09 18:44:09 +0530

    fix: remove the visual bell animation in wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -243,12 +243,14 @@ table.insert(config.hyperlink_rules, {
 --                           Bell
 -------------------------------------------------------------------------------
 config.audible_bell = "Disabled"
-config.visual_bell = {
-	fade_in_function = "EaseIn",
-	fade_in_duration_ms = 150,
-	fade_out_function = "EaseOut",
-	fade_out_duration_ms = 150,
-}
+-- config.visual_bell = {
+-- 	fade_in_function = "EaseIn",
+-- 	fade_in_duration_ms = 150,
+-- 	fade_out_function = "EaseOut",
+-- 	fade_out_duration_ms = 150,
+-- }
+
+--config.visual_bell = "Disabled"
 
 -------------------------------------------------------------------------------
 --                           Miscellaneous
